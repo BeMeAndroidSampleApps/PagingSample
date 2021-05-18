e12cac216cfdd4c472a2abdc5d042a04e07500e9
Hyun Woo Lee <l2hyunwoo@gmail.com>
2021-05-18 15:22:36 +0900

    [FEAT]  : 서버통신 Base 만들기

---
+++ app/src/main/java/com/beme/nunu/pagingsample/api/GithubService.kt
@@ -0,0 +1,51 @@
+package com.beme.nunu.pagingsample.api
+
+import com.beme.nunu.pagingsample.model.Repo
+import com.google.gson.annotations.SerializedName
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import okhttp3.logging.HttpLoggingInterceptor.Level
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface GithubService {
+    /**
+     * Get repos ordered by stars.
+     */
+    @GET("search/repositories?sort=stars")
+    suspend fun searchRepos(
+        @Query("q") query: String,
+        @Query("page") page: Int,
+        @Query("per_page") itemsPerPage: Int
+    ): RepoSearchResponse
+
+    companion object {
+        private const val BASE_URL = "https://api.github.com/"
+
+        fun create(): GithubService {
+            val logger = HttpLoggingInterceptor()
+            logger.level = Level.BASIC
+
+            val client = OkHttpClient.Builder()
+                .addInterceptor(logger)
+                .build()
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(GithubService::class.java)
+        }
+    }
+
+    /**
+     * Data class to hold repo responses from searchRepo API calls.
+     */
+    data class RepoSearchResponse(
+        @SerializedName("total_count") val total: Int = 0,
+        @SerializedName("items") val items: List<Repo> = emptyList(),
+        val nextPage: Int? = null
+    )
+}
---
+++ app/src/main/java/com/beme/nunu/pagingsample/model/Repo.kt
@@ -0,0 +1,29 @@
+package com.beme.nunu.pagingsample.model
+
+import com.google.gson.annotations.SerializedName
+
+/**
+ * Immutable model class for a Github repo that holds all the information about a repository.
+ * Objects of this type are received from the Github API, therefore all the fields are annotated
+ * with the serialized name.
+ * This class also defines the Room repos table, where the repo [id] is the primary key.
+ */
+data class Repo(
+    @field:SerializedName("id") val id: Long,
+    @field:SerializedName("name") val name: String,
+    @field:SerializedName("full_name") val fullName: String,
+    @field:SerializedName("description") val description: String?,
+    @field:SerializedName("html_url") val url: String,
+    @field:SerializedName("stargazers_count") val stars: Int,
+    @field:SerializedName("forks_count") val forks: Int,
+    @field:SerializedName("language") val language: String?
+)
+
+/**
+ * RepoSearchResult from a search, which contains List<Repo> holding query data,
+ * and a String of network error state.
+ */
+sealed class RepoSearchResult {
+    data class Success(val data: List<Repo>) : RepoSearchResult()
+    data class Error(val error: Exception) : RepoSearchResult()
+}
